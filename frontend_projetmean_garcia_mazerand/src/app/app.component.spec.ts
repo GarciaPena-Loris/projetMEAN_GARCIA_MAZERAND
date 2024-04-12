f4da68181391a7ef938b949d54359dd29e5ba08c
Elliot-mazerand <elliotmazerand@gmail.com>
2024-04-12 20:28:29 +0200

    [front-end] début front

--- frontend_projetmean_garcia_mazerand/src/app/app.component.spec.ts
+++ frontend_projetmean_garcia_mazerand/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'waterBnB_frontend' title`, () => {
+  it(`should have the 'frontend_projetmean_garcia_mazerand' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('waterBnB_frontend');
+    expect(app.title).toEqual('frontend_projetmean_garcia_mazerand');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, waterBnB_frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend_projetmean_garcia_mazerand');
   });
 });
--- frontend_projetmean_garcia_mazerand/src/app/app.component.ts
+++ frontend_projetmean_garcia_mazerand/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SearchFormComponent} from "./search-form/search-form.component";
+import {HeaderComponent} from "./header/header.component";
+import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchFormComponent, HeaderComponent, MatFormField, MatFormFieldModule, MatLabel],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'waterBnB_frontend';
+  title = 'frontend_projetmean_garcia_mazerand';
 }
